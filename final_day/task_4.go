8bc08f20407158d2e51d4c468155da4480adc836
ScareTrow <75736890+ScareTrow@users.noreply.github.com>
2022-02-21 21:14:06 +0600

    Last day tasks

---
+++ final_day/task_4.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var h, w int
+
+type board []byte
+
+func (b board) String() string {
+	var sb strings.Builder
+	for i := 0; i < h; i++ {
+		sb.WriteString(string(b[i*w : (i+1)*w]))
+		sb.WriteByte('\n')
+	}
+	sb.WriteString(string(b[(h-2)*w : (h-1)*w]))
+
+	return sb.String()
+}
+
+func clone(b board) board {
+	return append(b[:0:0], b...)
+}
+
+func main() {
+	var cur board
+
+	nxt := make(board, 0, 32768)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		bytes := input.Bytes()
+		nxt = append(nxt, bytes...)
+		h, w = h+1, len(bytes)
+	}
+
+	s, n := 0, 1 // при каждом шаге измененить значения
+	for n > 0 {
+		s++
+		n = 0
+		cur = clone(nxt)
+		for j := 0; j < h; j++ {
+			for i := 0; i < w; i++ {
+				x := (i + 1) % w
+				if cur[j*w+i] == '>' && cur[j*w+x] == '.' {
+					nxt[j*w+i], nxt[j*w+x] = nxt[j*w+x], nxt[j*w+i]
+					n++
+				}
+			}
+		}
+		cur = clone(nxt)
+		for j := 0; j < h; j++ {
+			y := (j + 1) % h
+			for i := 0; i < w; i++ {
+				if cur[j*w+i] == 'v' && cur[y*w+i] == '.' {
+					nxt[j*w+i], nxt[y*w+i] = nxt[y*w+i], nxt[j*w+i]
+					n++
+				}
+			}
+		}
+	}
+	fmt.Println(s)
+}
